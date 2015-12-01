9fabc8909f69f166b21451d989cfb56aa3ccf528
Daniel <daanielvb@gmail.com>
2015-12-01 19:29:53 -0200

    Funcionamento da RBF refatorado - FALTA CORRIGIR

--- src/rbf/RBF.java
+++ src/rbf/RBF.java
@@ -7,22 +7,51 @@ import kmeans.AuxiliaryFunctions;
 import kmeans.Cluster;
 import kmeans.Kmeans;
 import kmeans.Ponto;
+import pso.Particula;
 
 
 public class RBF {
 	private List<Cluster> rbfs;
+	private LeitorEntradaRBF leitorEntrada;
+	
+
 	private List<Double> intermediateWeights;
 	private int count;
 	private int fitness;
 	
 	
-	public RBF(Kmeans k){
+	public RBF(Kmeans k, LeitorEntradaRBF leitor){
 		this.rbfs = k.getClusters();
 		this.count = 0;
 		this.fitness = 0;
-		this.intermediateWeights = new ArrayList<Double>();
+		this.leitorEntrada = leitor;		
 	}
 	
+	
+	/*
+	 * Varre a base de entrada e o numero 
+	 * de neuronios para calcular os pesos intermediarios 
+	 * e posteriormente o % de erro
+	 */
+	public void calculateErrorPercentage(Particula p, int numNeuronios){
+		double erro = 0;
+		for (int j = 0; j < this.getLeitorEntrada().getConjuntoCaracteristicas().size(); j++) {
+			// Array com valores temporarios de Y
+			ArrayList<Double> Y = new ArrayList<Double>();
+			for (int i = 0; i < numNeuronios; i++) {
+				Y.add(AuxiliaryFunctions.calculateRBFWeights(this.getRbfs().get(i).getCenter(), // c 
+					this.getLeitorEntrada().getConjuntoCaracteristicas().get(j), this.getRbfs().get(i).getVariance()));							
+			}
+			// Somatorio das multiplicacoes de Y x W
+			double somatorio = 0;
+			for (int z = 0; z < Y.size(); z++) {
+				somatorio += p.posicao[z] * Y.get(z);
+			}
+			erro += this.getLeitorEntrada().getBaseEntrada().get(j).get(4) - somatorio;
+			
+		}
+		System.out.println(erro/150);
+	}
 	/*
 	 * Calculate intermediate Weights
 	 */
@@ -108,5 +137,14 @@ public class RBF {
 	public void setIntermediateWeights(List<Double> intermediateWeights) {
 		this.intermediateWeights = intermediateWeights;
 	}
+	
+	public LeitorEntradaRBF getLeitorEntrada() {
+		return leitorEntrada;
+	}
+
+	public void setLeitorEntrada(LeitorEntradaRBF leitorEntrada) {
+		this.leitorEntrada = leitorEntrada;
+	}
+
 	
 }
--- src/rbf/TestClass.java
+++ src/rbf/TestClass.java
@@ -1,11 +1,9 @@
 package rbf;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 import kmeans.Kmeans;
-import kmeans.Ponto;
+import pso.Particula;
 
 public class TestClass {
 
@@ -15,35 +13,14 @@ public class TestClass {
 		le.converterBaseParaPonto();
 		Kmeans k = new Kmeans();
 		k.kmeans(4, le.getConjuntoCaracteristicas());
-		RBF rbf = new RBF(k);
-		Ponto p = new Ponto(0.1,0.123,0.3,0.2);
-		System.out.println(rbf.getRbfs().get(0).getVariance());
-		System.out.println(rbf.getRbfs().get(0).getCenter().printCoordinates());
-		rbf.calculateRBFWeights(p);
-		List <Double> finalWeights = new ArrayList<Double>();
-		finalWeights.add(0.23);
-		finalWeights.add(0.77);
-		finalWeights.add(0.221);
-		finalWeights.add(0.99);
-		System.out.println(rbf.calculateRBFOutput(le.getBaseEntrada().get(0).get(4),rbf.getIntermediateWeights(), finalWeights));
+		RBF rbf = new RBF(k,le);
+		Particula p = new Particula(4);
+		rbf.calculateErrorPercentage(p, 4);
 		
+		
+		
+	//	System.out.println(rbf.getLeitorEntrada().getConjuntoCaracteristicas().size());
 
-//		
-//		
-
-//		
-//		System.out.println(k.printAllClustersCenter());
-//		System.out.println(k.printAllClustersVariance());
-//		
-//		System.out.println(k.getClusters().get(0).getContains().size());
-//		System.out.println(k.getClusters().get(1).getContains().size());
-//		System.out.println(k.getClusters().get(2).getContains().size());
-//		System.out.println(k.getClusters().get(3).getContains().size());
-		
-		
-		
-//		Pso pso = new Pso(5,20,3);
-//		System.out.println(pso.gBest);
 	}
 
 }
